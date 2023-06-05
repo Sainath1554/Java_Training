d6dd421e792d5cb28a2e3afb5107b3a49832c08e
Sai <sainathkorlakunta958@gmail.com>
2023-06-04 20:48:12 -0500

    delete_all

--- src/Day03/Vehicle.java
+++
@@ -1,9 +0,0 @@
-package Day03;
-
-public class Vehicle {
-    private int model;
-    public String engineType()
-    {
-        return "Petrol";
-    }
-}
--- src/Male.java
+++
@@ -1,6 +0,0 @@
-public class Male extends Person{
-    @Override
-    void walk() {
-        System.out.println("Male is walking");
-    }
-}
