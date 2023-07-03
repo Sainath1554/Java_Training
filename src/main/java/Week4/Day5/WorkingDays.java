1ad545afb3239c5dc222d265cbf422901f08651c
Sai <sainathkorlakunta958@gmail.com>
2023-07-03 10:57:52 -0500

    DateTiem

--- src/main/java/Week4/Day5/WorkingDays.java
+++ src/main/java/Week4/Day5/WorkingDays.java
@@ -6,5 +6,16 @@ import java.time.temporal.ChronoUnit;
 
 public class WorkingDays {
     public static void main(String[] args) {
+        LocalDate specificDate = LocalDate.of(2023, 7, 10);
+        LocalDate endOfMonth = specificDate.withDayOfMonth(specificDate.lengthOfMonth());
+        int workingDays = 0;
+        for(LocalDate currentDate = specificDate;!currentDate.isAfter(endOfMonth);currentDate = currentDate.plusDays(1))
+        {
+            if(currentDate.getDayOfWeek()!=DayOfWeek.SATURDAY && currentDate.getDayOfWeek()!=DayOfWeek.SUNDAY)
+            {
+                workingDays+=1;
+            }
+        }
+        System.out.println(workingDays);
     }
 }
